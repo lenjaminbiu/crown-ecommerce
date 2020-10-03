dc83dc9bfece331f46a40148b032ffcc2362a760
Benjamin <lenjaminbiu@gmail.com>
2020-10-03 11:48:48 +1000

    finished homepage and styling menu-item component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./homepage.component";
-import "./homepage.scss";
+import HomePage from "./pages/homepage/homepage.component";
+import "./pages/homepage/homepage.scss";
 
 function App() {
   return (
